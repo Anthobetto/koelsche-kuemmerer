13df20e91f0ad674e0edd523fc5980ce670bfa36
Anthony Flores <anthonyflores@Ordenador-portatil-de-Anthony.local>
2025-05-04 13:14:33 +0200

    updates

--- src/js/pages/WelcomePage.jsx
+++ src/js/pages/WelcomePage.jsx
@@ -14,15 +14,15 @@ export const WelcomePage = () => {
 				<div className="container text-center pt-5 pb-5">
 					<motion.div
 						initial={{ scale: 0.8, opacity: 0 }}
-						animate={{ scale: 1, opacity: 1 }}
+						animate={{ scale: 1.3, opacity: 1 }}
 						transition={{ duration: 0.8, ease: "easeOut" }}
 						className="d-flex justify-content-center align-items-center mb-4"
 					>
 						<img
-							src="/images/backgroud.png"
+							src="images/background.png"
 							alt="Köllsche Klümmerer Team"
 							className="img-fluid"
-							style={{ maxWidth: "600px", width: "100%" }}
+							style={{ maxWidth: "600px", width: "100%", marginBottom: "3rem" }} 
 						/>
 					</motion.div>
 
@@ -33,9 +33,9 @@ export const WelcomePage = () => {
 						className="text-center"
 					>
 						<h2 className="text-white display-4">
-							WILLKOMMEN AUF UNSERER WEB-SITE!
+							WILLKOMMEN AUF UNSERER WEB-SEITE!
 						</h2>
-						<p className="text-white">SCHÖN DAT DE DO BES!</p>
+						<h4 className="text-white">SCHÖN DAT DE DO BES!</h4>
 					</motion.div>
 				</div>
 			</section>
